02e420ba036214c9d5e67d6936ed1536c9c26497
Alessio Michelini <darkmavis1980@gmail.com>
2024-02-04 16:15:27 +0000

    feat:  update setupscript

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ filePath = './conf/conf.ini'
 def createConfig():
     config = configparser.ConfigParser()
 
+    print("Creating MySQL Connection...")
     host = input('Please enter host: (defaults to localhost)').strip() or 'localhost'
     username = input('Please enter username:').strip() or ''
     password = input('Please enter password:').strip() or ''
@@ -21,6 +22,18 @@ def createConfig():
     config.set('DATABASE', 'PASSWORD', password)
     config.set('DATABASE', 'DB', dbName)
     config.set('DATABASE', 'PORT', port)
+
+    print("Creating InfluxDB Connection...")
+    influx_host = input('Please enter host: (defaults to localhost)').strip() or 'localhost'
+    influx_token = input('Please enter access token:').strip() or ''
+    influx_bucket = input('Please enter bucket:').strip() or ''
+    influx_org = input('Please enter organization:').strip() or ''
+
+    config.add_section('INFLUXDB')
+    config.set('INFLUXDB', 'HOST', influx_host)
+    config.set('INFLUXDB', 'TOKEN', influx_token)
+    config.set('INFLUXDB', 'BUCKET', influx_bucket)
+    config.set('INFLUXDB', 'ORG', influx_org)
 
     with open(filePath, 'w') as configfile:
         config.write(configfile)
