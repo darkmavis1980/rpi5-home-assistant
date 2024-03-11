07aeb6b86a289b4005b54f53e3ea15e567b11f6f
Alessio Michelini <darkmavis1980@gmail.com>
2024-03-11 10:58:24 +0000

    fix: updated docker script

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ docker stop raspi-temp
 
 docker build -t raspi-temp .
 
-docker run --name raspi-temp --rm -d -p 8000:8000 raspi-temp
+docker run --name raspi-temp -d -p 8000:8000 --restart unless-stopped raspi-temp
