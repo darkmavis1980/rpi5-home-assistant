4448c6adc719da438eb9063aaa59d5afb3028119
Alessio Michelini <darkmavis1980@gmail.com>
2024-08-06 23:47:35 +0100

    feat: add healthcheck endpoint

---
+++ models/utils.py
@@ -0,0 +1,7 @@
+"""Utils library"""
+from pydantic import BaseModel
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
